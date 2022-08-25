9776e9b61627f293d915f1860e29879ce4fbe9dc
Vedant Chaudhari <vedantchaudhari@outlook.com>
2022-08-25 17:00:41 -0400

    Initial Commit

---
+++ SummusTakeHome/SummusTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummusTakeHomeApp.swift
+//  SummusTakeHome
+//
+//  Created by Vedant Chaudhari on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SummusTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
